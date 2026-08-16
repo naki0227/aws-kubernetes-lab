89bbb22395e3d999284ec1be69516700ddad77f9
長瀬 唯楓　（Ibuki Nagase) <nakinakipal@gmail.com>
2026-08-16 22:36:23 +0900

    ci: add GitHub Actions OIDC authentication

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -168,3 +168,117 @@ resource "aws_eks_pod_identity_association" "load_balancer_controller" {
     aws_iam_role_policy_attachment.load_balancer_controller
   ]
 }
+
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+
+    principals {
+      type = "Federated"
+      identifiers = [
+        aws_iam_openid_connect_provider.github_actions.arn
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        "repo:naki0227/aws-kubernetes-lab:ref:refs/heads/main"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions_deploy" {
+  name               = "${var.cluster_name}-github-actions-deploy"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+}
+
+resource "aws_iam_role_policy" "github_actions_deploy" {
+  name = "${var.cluster_name}-github-actions-deploy"
+  role = aws_iam_role.github_actions_deploy.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:CompleteLayerUpload",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+
+        Resource = [
+          aws_ecr_repository.frontend.arn,
+          aws_ecr_repository.backend.arn
+        ]
+      },
+      {
+        Effect = "Allow"
+
+        Action = [
+          "eks:DescribeCluster"
+        ]
+
+        Resource = module.eks.cluster_arn
+      }
+    ]
+  })
+}
+
+resource "aws_eks_access_entry" "github_actions_deploy" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = aws_iam_role.github_actions_deploy.arn
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "github_actions_deploy" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = aws_iam_role.github_actions_deploy.arn
+
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSEditPolicy"
+
+  access_scope {
+    type       = "namespace"
+    namespaces = ["default"]
+  }
+
+  depends_on = [
+    aws_eks_access_entry.github_actions_deploy
+  ]
+}
--- terraform/dev/outputs.tf
+++ terraform/dev/outputs.tf
@@ -21,3 +21,7 @@ output "frontend_ecr_repository_url" {
 output "backend_ecr_repository_url" {
   value = aws_ecr_repository.backend.repository_url
 }
+
+output "github_actions_deploy_role_arn" {
+  value = aws_iam_role.github_actions_deploy.arn
+}
