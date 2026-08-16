e7cdb61597eadbc50e26eecc36b87f2c291f160b
長瀬 唯楓　（Ibuki Nagase) <nakinakipal@gmail.com>
2026-08-16 23:54:39 +0900

    fix: use environment AWS credentials

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -4,12 +4,6 @@ variable "aws_region" {
   default     = "ap-northeast-1"
 }
 
-variable "aws_profile" {
-  description = "Local AWS CLI profile"
-  type        = string
-  default     = "eks-lab"
-}
-
 variable "cluster_name" {
   description = "EKS cluster name"
   type        = string
